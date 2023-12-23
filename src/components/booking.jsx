31fd0407f1b6f381813136d0e95fe2a87ccc8045
TriDuong070803 <132152293+TriDuong070803@users.noreply.github.com>
2023-12-23 23:12:22 +0700

    Making Booking page

---
+++ src/components/booking.jsx
@@ -0,0 +1,11 @@
+import "../css/Booking.css"
+
+export default function Booking(){
+    return(
+        <div>
+            <h1>Book a Session</h1>
+            <p>Leave your information here and we will reach out to you ASAP.</p>
+            <div class="box"></div>
+        </div>
+    )
+}
