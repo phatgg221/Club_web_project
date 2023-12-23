9975c12438986403d91e7076210d8958f827532e
Duong Phu Dong <dongduong840@gmail.com>
2023-12-23 22:51:33 +0700

    Upload Card Hover

---
+++ src/components/Card.jsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import "./Card.css";
+
+const Card = ({ logo, organizer, title, description, imageUrl, linkUrl }) => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  useEffect(() => {
+    const card = document.getElementById("cardContent");
+    if (card) {
+      setShowScroll(card.scrollHeight > card.clientHeight);
+    }
+  }, [description]);
+
+  return (
+    <div className="card">
+      <a href={linkUrl} className="card-link"></a>
+      <div className="card-content" id="cardContent">
+        <span>
+          <img src={logo} />
+          {organizer}
+        </span>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+      {showScroll && <div className="scroll-indicator">Scroll</div>}
+      <div
+        className="card-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+    </div>
+  );
+};
+
+export default Card;
