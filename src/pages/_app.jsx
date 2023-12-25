1a87e6ade2a67de177ce2c0c81335119bcd84cf1
Nguyen Le Thu Nhan <102719300+NhanNguyen20@users.noreply.github.com>
2023-12-25 23:03:59 +0700

    add font

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,17 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.global.css';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import dynamic from 'next/dynamic';
-
+import { Montserrat } from 'next/font/google';
 // const Bootstrap = dynamic(() => import('bootstrap'), { ssr: false });
+
+const montserrat = Montserrat({ weight: '400', subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-    
+    <main className={montserrat.className}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </main>
   );
 }
 
