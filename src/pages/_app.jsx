b4b200896a81061d714a1ed1bfa9df500208d4a6
Vearce <hoangduc.vn@gmail.com>
2023-12-28 14:02:12 +0700

    Re-designed mobile nav-bar. Fixed layout shift bug.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,20 +1,29 @@
 // src/pages/_app.jsx
-import '../styles/global.css'; // Import global styles here
-import 'bootstrap/dist/css/bootstrap.min.global.css';
-import Header from '@/components/Header/Header';
-import Footer from '@/components/Footer/Footer';
-import dynamic from 'next/dynamic';
-import { Montserrat } from 'next/font/google';
+import "../styles/global.css"; // Import global styles here
+import "bootstrap/dist/css/bootstrap.min.global.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import dynamic from "next/dynamic";
+import { Montserrat } from "next/font/google";
 // const Bootstrap = dynamic(() => import('bootstrap'), { ssr: false });
 
-const montserrat = Montserrat({ weight: '400', subsets: ['latin'] });
+const montserrat = Montserrat({ weight: "400", subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
   return (
     <main className={montserrat.className}>
       <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <div
+        className="mainBody"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "calc(100vh - 70px)",
+        }}
+      >
+        <Component {...pageProps} />
+        <Footer />
+      </div>
     </main>
   );
 }
