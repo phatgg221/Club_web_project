4afbd565cdf2aa7bf9114b3fa7811d220ee1dd94
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-01-23 19:54:54 +0700

    mm

--- src/components/cardHover/Cards.jsx
+++ src/components/cardHover/Cards.jsx
@@ -1,9 +1,64 @@
 // Cards.jsx
 import React from "react";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 // import "@/styles/Card.module.css"; // Import specific component styles
 
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       // Fetch data for ongoing competitions
+//       const competitionsResponse = await fetch('/api/card_api');
+//       const competitionsData = await competitionsResponse.json();
+//       setOngoingCompetitions(competitionsData);
+
+//       // Fetch data for ongoing events
+//       const eventsResponse = await fetch('/api/ongoing_events');
+//       const eventsData = await eventsResponse.json();
+//       setOngoingEvents(eventsData);
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   };
+
+//   fetchData();
+// }, []);
+// console.log(fetchData);
 const Cards = () => {
+  const [ongoingCompetitions, setOngoingCompetitions] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch data for ongoing competitions
+        const competitionsResponse = await fetch('/api/card_api');
+        const competitionsData = await competitionsResponse.json();
+        // console.log("CompetitionResponse:", competitionsResponse);
+        // console.log("CompetitionData:", competitionsData);
+        setOngoingCompetitions(competitionsData);
+        // setOngoingCompetitions(competitionsData);
+
+        // const eventsResponse = await fetch('/api/ongoing_events');
+        // const eventsData = await eventsResponse.json();
+        // console.log("EventsResponse:", eventsResponse);
+        // console.log("EventsData:", eventsData);
+  
+        // Uncomment the next two lines if you want to set the data in state
+        // setOngoingEvents(eventsData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
+  if ('data' in ongoingCompetitions && 'mongoData' in ongoingCompetitions.data && ongoingCompetitions.data.mongoData.length > 0) {
+    console.log("Organizer: ", ongoingCompetitions.data.mongoData[0].organizer);
+   } else {
+    console.log("No organizer found.");
+   }
+   
   return (
     <div className="app">
       <div className="competition-section">
