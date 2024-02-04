ddf05c067f8fc4a56db2208118c0504518b42faf
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-02-04 18:45:56 +0700

    tips fetch data

--- src/components/Foldables/FoldableItem.jsx
+++ src/components/Foldables/FoldableItem.jsx
@@ -1,9 +1,14 @@
 import styles from "@/styles/Foldable.module.css";
 import React, { useState } from "react";
 import Link from "next/link";
-export default function FoldableItem({ title,year,author,link }) {
+import Popup from "reactjs-popup";
+import Content from "./content";
+import style from "@/styles/content.module.css";
+const FoldableItem = ({ title, year, author, link, isTip, tips }) => {
+  // State for tracking fold/unfold state
   const [isFolded, setIsFolded] = useState(true);
 
+  // Function to toggle fold/unfold state
   const handleToggleFold = () => {
     setIsFolded(!isFolded);
   };
@@ -11,7 +16,10 @@ export default function FoldableItem({ title,year,author,link }) {
   return (
     <div className={styles.item}>
       <div className={styles.innerContainer}>
-        <h3 className={styles.title}>{title}</h3>
+        {/* Displaying title based on whether it's a tip or not */}
+        <h3 className={styles.title}>{isTip ? tips.tipName : title}</h3>
+        {isTip && tips.tipsLink && <Link href={tips.tipsLink}>Link</Link>} 
+        {/* Fold/Unfold button */}
         <div
           className={`${styles.foldableContainer} ${
             isFolded ? styles.folded : ""
@@ -22,17 +30,53 @@ export default function FoldableItem({ title,year,author,link }) {
           </button>
         </div>
       </div>
-      <div
+
+      {/* Displaying additional content if it's not a tip */}
+      {!isTip && (
+        <div
+          className={`${styles.contentContainer} ${
+            isFolded ? styles.foldedContent : ""
+          }`}
+        >
+          <div className={styles.content}>
+          
+            <span>{year}</span>
+            <span>{author}</span>
+
+            {/* Displaying link if available */}
+            {link && <Link href={link}>Link</Link>}
+          </div>
+        </div>
+      )}
+      
+
+      {isTip && tips.realContent && tips.realContent.map((content, index) => (
+        <div key={index} className={`${styles.contentContainer} ${isFolded ? styles.foldedContent : ""}`}>
+          <div className={styles.content}>
+            <h4>{content.name}</h4>
+            <Popup modal trigger={<button className={styles.foldButton}>View</button>}>
+              {close => <Content close={close} className={style.modal} content={content} />}
+            </Popup>
+          </div>
+        </div>
+      ))}
+
+      {/* {isTip &&(
+        <div
         className={`${styles.contentContainer} ${
           isFolded ? styles.foldedContent : ""
         }`}
       >
         <div className={styles.content}>
-        <span>{year}</span>
+          <span></span>
           <span>{author}</span>
-          <Link href={link}>Link</Link>
+
+          
         </div>
-      </div>
+      </div> 
+      )} */}
     </div>
   );
-}
+};
+
+export default FoldableItem;
---
+++ src/components/Foldables/content.jsx
@@ -0,0 +1,19 @@
+//content.js
+import React from "react";
+import Image from "next/image";
+import style from "@/styles/content.module.css"
+export default ({ close, content }) => (
+  <div className={style.modal}>
+    <a className={style.close} onClick={close}>
+      ×
+    </a>
+    <div className={style.header}> {content.name}</div>
+    <div className={style.content}>
+      {" "}
+      
+      <p>{content.contents}</p>
+      <p><img src={content.tipImage} alt="" width={500} height={300}/></p>
+
+    </div>
+  </div>
+);
