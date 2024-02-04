bf926d538a83122b491e59749d1fd457e6b71621
TaiVanNgo <vantaingo.056@gmail.com>
2024-02-04 21:49:19 +0700

    Admin: header admin and card view front end

--- src/pages/admin/Card/view.jsx
+++ src/pages/admin/Card/view.jsx
@@ -2,88 +2,106 @@ import { useEffect, useState } from "react";
 import React from "react";
 import style from "@/styles/table.module.css";
 import AdminHeader from "@/components/Header/adminHeader";
-const CardTable= () => {
-    const [ongoingCompetitions, setOngoingCompetitions] = useState([]);
+const CardTable = () => {
+  const [ongoingCompetitions, setOngoingCompetitions] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const competitionsResponse = await fetch('/api/card_api');
-                const competitionsData = await competitionsResponse.json();
-                setOngoingCompetitions(competitionsData);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const handleDelete= async(card)=>{
-        // console.log("card id "+ card._id)
-        try{
-            const response = await fetch(`/api/card_api?id=${card._id}`,{
-                method: 'DELETE',
-            });
-
-            if(response.ok){
-                console.log("Card delete successful");
-                window.location.reload();
-            }else{
-                console.error('Failed to delete card');
-            }
-        }catch(error){
-            console.error("Error deleting card", error);
-        }
-    }
-    const handleReturnBut= async() =>{
-        window.location.href= '/admin/dashboard/view';
-    }
-
-    const handleCreateButton = async () => {
-        window.location.href = '/admin/Card/form';
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const competitionsResponse = await fetch("/api/card_api");
+        const competitionsData = await competitionsResponse.json();
+        setOngoingCompetitions(competitionsData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
-    
-    const handleUpdateButton = async (id) => {
-        console.log("ID to update "+ id);
-        window.location.href = `/admin/Card/form?id=${id}`;
-    };
-    
-    return (
-        <>
-            <AdminHeader/>
-            <table className={style.mainTable}>
-                <thead>
-                    <tr>
-                        <th>Organizer</th>
-                        <th>Competition name</th>
-                        <th>Location</th>
-                        <th>Link to web</th>
-                        <th>Image</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {ongoingCompetitions && ongoingCompetitions.data && ongoingCompetitions.data.mongoData && ongoingCompetitions.data.mongoData.map((item, index) => (
-                        <tr key={index}>
-                            <td>{item.organizer}</td>
-                            <td>{item.competitionName}</td>
-                            <td>{item.location}</td>
-                            <td><a href={item.linkToWeb}>Link</a></td>
-                            <td><img className={style.imageTable} src={item.imageURL} alt="Compete image" /></td>
-                            <td><button onClick={() => handleUpdateButton(item._id)}>Update</button><button onClick={() => handleDelete(item)}>Delete</button></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div className="button-container">
-                <button onClick={handleCreateButton}>Create new card evnet</button>
-                <button onClick={handleReturnBut}>Return</button>
-                
-            </div>
-            
-        </>
-    );
-}
+
+    fetchData();
+  }, []);
+
+  const handleDelete = async (card) => {
+    // console.log("card id "+ card._id)
+    try {
+      const response = await fetch(`/api/card_api?id=${card._id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        console.log("Card delete successful");
+        window.location.reload();
+      } else {
+        console.error("Failed to delete card");
+      }
+    } catch (error) {
+      console.error("Error deleting card", error);
+    }
+  };
+  const handleReturnBut = async () => {
+    window.location.href = "/admin/dashboard/view";
+  };
+
+  const handleCreateButton = async () => {
+    window.location.href = "/admin/Card/form";
+  };
+
+  const handleUpdateButton = async (id) => {
+    console.log("ID to update " + id);
+    window.location.href = `/admin/Card/form?id=${id}`;
+  };
+
+  return (
+    <>
+      <AdminHeader />
+      <div className={style.divTable}>
+        <table className={style.mainTable}>
+          <thead className={style.tableHeading}>
+            <tr>
+              <th>Organizer</th>
+              <th>Competition name</th>
+              <th>Location</th>
+              <th>Link to web</th>
+              <th>Image</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ongoingCompetitions &&
+              ongoingCompetitions.data &&
+              ongoingCompetitions.data.mongoData &&
+              ongoingCompetitions.data.mongoData.map((item, index) => (
+                <tr key={index} className={style.tableRow}>
+                  <td>{item.organizer}</td>
+                  <td>{item.competitionName}</td>
+                  <td>{item.location}</td>
+                  <td>
+                    <a href={item.linkToWeb}>Link</a>
+                  </td>
+                  <td>
+                    <img
+                      className={style.imageTable}
+                      src={item.imageURL}
+                      alt="Compete image"
+                    />
+                  </td>
+                  <td className={style.btnContainer}>
+                    <button className={style.btnUpdate} onClick={() => handleUpdateButton(item._id)}>
+                      Update
+                    </button>
+                    <button className={style.btnDelete} onClick={() => handleDelete(item)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+      <div className={style.btnCreateNewCardDiv}>
+        <button className={style.btnCreateCard} onClick={handleCreateButton}>
+          Create new card event
+        </button>
+        <button className={style.btnReturn} onClick={handleReturnBut}>Return</button>
+      </div>
+    </>
+  );
+};
 CardTable.hideLayout = true;
 export default CardTable;
--- src/pages/admin/dashboard/view.jsx
+++ src/pages/admin/dashboard/view.jsx
@@ -1,32 +1,47 @@
 import React from "react";
 import AdminHeader from "@/components/Header/adminHeader";
+import style from "@/styles/AdminDasboard.module.css";
 
-function Dashboard(){
-    const handleCardButton= async()=>{
-        window.location.href= '/admin/Card/view';
-    }
+function Dashboard() {
+  const handleCardButton = async () => {
+    window.location.href = "/admin/Card/view";
+  };
 
-    const handleChampButton= async()=>{
-        window.location.href= '/admin/champions/view';
-    }
-    const handleSample= async() =>{
-        window.location.href= '/admin/sample/view';
-    }
-    const hanldeMember= async() =>{
-        window.location.href= '/admin/member/view';
-    }
-    return(
-        <div>
-            <AdminHeader/>
-            <div>
-                <button onClick={handleCardButton}>Mange Card</button>
-                <button onClick={handleChampButton}>Manage Champion</button>
-                <button onClick={hanldeMember}>Manage Member</button>
-                <button onClick={handleSample}>Manage Sample</button>
-                <button>Manage Tips</button>
-            </div>
-        </div>
-    )
+  const handleChampButton = async () => {
+    window.location.href = "/admin/champions/view";
+  };
+  const handleSample = async () => {
+    window.location.href = "/admin/sample/view";
+  };
+  const hanldeMember = async () => {
+    window.location.href = "/admin/member/view";
+  };
+  return (
+    <div>
+      <AdminHeader />
+      <div className={style.container}>
+        <button
+          className={`${style.btn} ${style.btn1}`}
+          onClick={handleCardButton}
+        >
+          Manage Card
+        </button>
+        <button
+          className={`${style.btn} ${style.btn2}`}
+          onClick={handleChampButton}
+        >
+          Manage Champion
+        </button>
+        <button className={`${style.btn} ${style.btn3}`} onClick={hanldeMember}>
+          Manage Member
+        </button>
+        <button className={`${style.btn} ${style.btn4}`} onClick={handleSample}>
+          Manage Sample
+        </button>
+        <button>Manage Tips</button>
+      </div>
+    </div>
+  );
 }
-Dashboard.hideLayout= true;
+Dashboard.hideLayout = true;
 export default Dashboard;
