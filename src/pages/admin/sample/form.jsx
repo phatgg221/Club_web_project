a234da2b8c23a0fcb13866f8c3c7e2c97e247c45
TaiVanNgo <vantaingo.056@gmail.com>
2024-02-16 20:11:58 +0700

    Manage member, manage sample front end edit
    Edit front end for manage member and manage sample pages --> Edit form and view

--- src/pages/admin/sample/form.jsx
+++ src/pages/admin/sample/form.jsx
@@ -1,118 +1,171 @@
-import React, { useEffect,useState } from 'react';
-import { useRouter } from 'next/router';
-
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import style from "@/styles/Admin.Form.module.css";
+import styleBtn from "@/styles/table.module.css";
 
 const NewSampleForm = () => {
-    const router = useRouter();
-    const { id } = router.query;
+  const router = useRouter();
+  const { id } = router.query;
 
-    const [formData, setFormData] = useState({
-        sampleName: '',
-        sampleContents:'',
-        sampleAuthor: '',
-        sampleLink: '',
+  const [formData, setFormData] = useState({
+    sampleName: "",
+    sampleContents: "",
+    sampleAuthor: "",
+    sampleLink: "",
+  });
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  useEffect(() => {
+    if (id) {
+      const fetchData = async () => {
+        try {
+          setIsEditMode(true);
+          const response = await fetch(`/api/sample_api?id=${id}`);
+          const data = await response.json();
+          // console.log(JSON.stringify(data.data.sampleData) + "aksdjhasdkjashdkasdahsdmv,navkadljj");
+          if (
+            data &&
+            data.error === false &&
+            data.statusCode === 200 &&
+            data.data &&
+            data.data.sampleData &&
+            data.data.sampleData.data
+          ) {
+            setFormData(data.data.sampleData.data);
+          } else {
+            console.error("Data structure is not as expected: ", data);
+          }
+        } catch (error) {
+          console.error("Error fetching data: ", error);
+        }
+      };
+      fetchData();
+    }
+  }, [id]);
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const [isEditMode, setIsEditMode]= useState(false);
+  const hanldeReturn = () => {
+    window.location.href = "/admin/sample/view";
+  };
 
-    useEffect(() =>{
-        if(id){
-            const fetchData= async() =>{
-                try{
-                    setIsEditMode(true);
-                    const response= await fetch(`/api/sample_api?id=${id}`);
-                    const data= await response.json();
-                    // console.log(JSON.stringify(data.data.sampleData) + "aksdjhasdkjashdkasdahsdmv,navkadljj");
-                    if(data && data.error === false && data.statusCode === 200 && data.data && data.data.sampleData && data.data.sampleData.data){
-                        setFormData(data.data.sampleData.data);
-                    }else{
-                        console.error('Data structure is not as expected: ', data);
+  const hanldeSubmit = async (event) => {
+    event.preventDefault();
 
-                    }
-                }catch(error){
-                    console.error('Error fetching data: ', error);
-                }
-            };
-            fetchData();
+    fetch("/api/sample_api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network reponse was not ok");
         }
-    },[id]);
 
-    const handleInputChange= (event) =>{
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value,
-        });
-    };
+        window.location.href = "/admin/sample/view";
 
-    const hanldeReturn=() => {
-        window.location.href='/admin/sample/view';
+        return response.json();
+      })
+      .then((data) => console.log("Success: ", data))
+      .catch((error) => console.error("Error", error));
+  };
+
+  const handleUpdate = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`/api/sample_api?id=${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      window.location.href = "/admin/sample/view";
+    } catch (error) {
+      console.error("Error:", error);
     }
+  };
+  return (
+    <div className={style.formContainer}>
+      <form className={style.form} onSubmit={hanldeSubmit}>
+        <div className={style.inputGroup}>
+          <label>Sample name:</label>
+          <input
+            required={true}
+            type="text"
+            name="sampleName"
+            placeholder={isEditMode ? formData.sampleName : ""}
+            value={formData.sampleName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={style.Row}>
+          <div className={`${style.inputGroup} ${style.organizerInput}`}>
+            <label>Contents:</label>
+            <input
+              required={true}
+              type="text"
+              name="sampleContents"
+              placeholder={isEditMode ? formData.sampleContents : ""}
+              value={formData.sampleContents}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className={style.inputGroup}>
+            <label>Author:</label>
+            <input
+              required={true}
+              type="text"
+              name="sampleAuthor"
+              placeholder={isEditMode ? formData.sampleAuthor : ""}
+              value={formData.sampleAuthor}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className={style.inputGroup}>
+          <label>Link:</label>
+          <input
+            required={true}
+            type="text"
+            name="sampleLink"
+            placeholder={isEditMode ? formData.sampleLink : ""}
+            value={formData.sampleLink}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={styleBtn.btnBottomDiv}>
+          <button
+            className={`${styleBtn.btn} ${styleBtn.btnBottom} ${styleBtn.btnForm}`}
+            type="submit"
+            onClick={isEditMode ? handleUpdate : hanldeSubmit}
+          >
+            {isEditMode ? "Update" : "Create"}
+          </button>
+          <button
+            className={`${styleBtn.btn} ${styleBtn.btnBottom} ${styleBtn.btnForm}`}
+            type="button"
+            onClick={hanldeReturn}
+          >
+            {" "}
+            Return{" "}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-    const hanldeSubmit= async(event) =>{
-        event.preventDefault();
-
-        fetch('/api/sample_api',{
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-        .then(response =>{
-            if(!response.ok){
-                throw new Error ('Network reponse was not ok');
-            }
-
-            window.location.href= '/admin/sample/view';
-
-            return response.json();
-        })
-        .then(data=> console.log('Success: ', data))
-        .catch(error => console.error('Error', error));
-    };
-
-    const handleUpdate = async (event) => {
-        event.preventDefault();
-        try{
-            const response = await fetch(`/api/sample_api?id=${id}`,{
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-            if(!response.ok){
-                throw new Error('Network response was not ok');
-            }
-            window.location.href = '/admin/sample/view';
-        }catch(error){
-            console.error('Error:', error);
-        }
-    }
-    return(
-        <form onSubmit={hanldeSubmit}>
-            <label>
-                Sample name:
-                <input required={true} type='text' name="sampleName" placeholder={isEditMode?formData.sampleName:''} value={formData.sampleName} onChange={handleInputChange} />
-            </label>
-            <label>
-                Contents:
-                <input required= {true} type='text' name="sampleContents" placeholder={isEditMode? formData.sampleContents : ''} value={formData.sampleContents} onChange={handleInputChange} />
-            </label>
-            <label>
-                Author:
-                <input required= {true} type='text' name="sampleAuthor" placeholder={isEditMode? formData.sampleAuthor : ''} value={formData.sampleAuthor} onChange={handleInputChange} /> 
-            </label>
-            <label>
-                Link:
-                <input required= {true} type='text' name="sampleLink" placeholder={isEditMode? formData.sampleLink : ''} value={formData.sampleLink} onChange={handleInputChange} />
-            </label>
-            <button type= "submit" onClick={isEditMode ? handleUpdate : hanldeSubmit}>{isEditMode? 'Update' : 'Create'}</button>
-            <button type= "button" onClick={hanldeReturn}> Return </button>
-        </form>
-    )
-}
-
-
-NewSampleForm.hideLayout= true;
+NewSampleForm.hideLayout = true;
 export default NewSampleForm;
