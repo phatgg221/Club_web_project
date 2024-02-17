ea965f4654ed709d2332bd59ac465adc1398181d
TaiVanNgo <vantaingo.056@gmail.com>
2024-02-17 18:23:48 +0700

    TipPage front end
    Edit front end for view and form in page "tip"

--- src/pages/admin/tips/form.jsx
+++ src/pages/admin/tips/form.jsx
@@ -1,38 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import style from "@/styles/Admin.Form.module.css";
+import styleBtn from "@/styles/table.module.css";
 
 const NewTipForm = () => {
   const router = useRouter();
   const { id } = router.query;
   const [contentCount, setContentCount] = useState(1);
-  const [contents, setContents] = useState([{ name: '', contents: '', tipImage: '' }]);
+  const [contents, setContents] = useState([
+    { name: "", contents: "", tipImage: "" },
+  ]);
   const [isEditMode, setIsEditMode] = useState(false);
   const [formData, setFormData] = useState({
-    tipName: '',
-    tipsLink: '',
+    tipName: "",
+    tipsLink: "",
     realContent: [],
   });
 
   useEffect(() => {
-    setFormData(prevState => ({ ...prevState, realContent: contents }));
+    setFormData((prevState) => ({ ...prevState, realContent: contents }));
   }, [contents]);
-  
-  useEffect(() =>{
+
+  useEffect(() => {
     const fetchData = async () => {
-      if(id){
-        try{
+      if (id) {
+        try {
           setIsEditMode(true);
           const response = await fetch(`/api/tip_api?id=${id}`);
           const data = await response.json();
-          if(data && data.error === false && data.statusCode === 200 && data.data && data.data.sampleData && data.data.sampleData.data){
+          if (
+            data &&
+            data.error === false &&
+            data.statusCode === 200 &&
+            data.data &&
+            data.data.sampleData &&
+            data.data.sampleData.data
+          ) {
             setFormData(data.data.sampleData.data);
             setContentCount(data.data.sampleData.data.realContent.length);
             setContents(data.data.sampleData.data.realContent);
           } else {
-            console.error('Data structure is not as expected: ', data);
+            console.error("Data structure is not as expected: ", data);
           }
-        } catch(error) {
-          console.error('Error fetching data: ', error);
+        } catch (error) {
+          console.error("Error fetching data: ", error);
         }
       }
     };
@@ -47,11 +58,11 @@ const NewTipForm = () => {
   const handleInputChangeUpdate = (index, event) => {
     const newContents = [...formData.realContent];
     newContents[index][event.target.name] = event.target.value;
-    setFormData(prevState => ({ ...prevState, realContent: newContents }));
+    setFormData((prevState) => ({ ...prevState, realContent: newContents }));
   };
   const handleAddContent = () => {
     setContentCount(contentCount + 1);
-    setContents([...contents, { name: '', contents: '', tipImage: '' }]);
+    setContents([...contents, { name: "", contents: "", tipImage: "" }]);
   };
 
   const handleDelete = (index) => {
@@ -61,26 +72,26 @@ const NewTipForm = () => {
   };
 
   const hanldeReturn = () => {
-    window.location.href = '/admin/tips/tips';
+    window.location.href = "/admin/tips/tips";
   };
 
   const hanldeSubmit = async (event) => {
     event.preventDefault();
-    setFormData(prevState => ({ ...prevState, realContent: contents }));
+    setFormData((prevState) => ({ ...prevState, realContent: contents }));
     try {
-      const response = await fetch('/api/tip_api', {
-        method: 'POST',
+      const response = await fetch("/api/tip_api", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
-      window.location.href = '/admin/tips/tips';
+      window.location.href = "/admin/tips/tips";
     } catch (error) {
-      console.error('Error', error);
+      console.error("Error", error);
     }
   };
 
@@ -88,123 +99,166 @@ const NewTipForm = () => {
     event.preventDefault();
     try {
       const response = await fetch(`/api/tip_api?id=${id}`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
-      window.location.href = '/admin/tips/tips';
+      window.location.href = "/admin/tips/tips";
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   return (
-    <form onSubmit={hanldeSubmit}>
-      <label>
-        Tip Name:
-        <input
-          required={true}
-          type='text'
-          name='tipName'
-          value={formData.tipName}
-          placeholder={isEditMode ? formData.tipName : ''}
-          onChange={(e) => setFormData({ ...formData, tipName: e.target.value })}
-        />
-      </label>
-      <label>
-        Tip Link:
-        <input
-          required={true}
-          type='text'
-          name='tipsLink'
-          value={formData.tipsLink}
-          placeholder={isEditMode ? formData.tipsLink : ''}
-          onChange={(e) => setFormData({ ...formData, tipsLink: e.target.value })}
-        />
-      </label>
+    <div className={`${style.formContainer} ${style.formContainerTips}`}>
+      <form
+        className={`${style.form} ${style.formTips}`}
+        onSubmit={hanldeSubmit}
+      >
+        <div className={style.inputGroup}>
+          <label>Tip Name:</label>
+          <input
+            required={true}
+            type="text"
+            name="tipName"
+            value={formData.tipName}
+            placeholder={isEditMode ? formData.tipName : ""}
+            onChange={(e) =>
+              setFormData({ ...formData, tipName: e.target.value })
+            }
+          />
+        </div>
+        <div className={style.inputGroup}>
+          <label>Tip Link:</label>
+          <input
+            required={true}
+            type="text"
+            name="tipsLink"
+            value={formData.tipsLink}
+            placeholder={isEditMode ? formData.tipsLink : ""}
+            onChange={(e) =>
+              setFormData({ ...formData, tipsLink: e.target.value })
+            }
+          />
+        </div>
 
-      {!isEditMode && contents.map((content, index) => (
-        <div key={index}>
-          <label>
-            Content Name:
-            <input
-              type='text'
-              name='name'
-              value={content.name}
-              onChange={(e) => handleInputChange(index, e)}
-            />
-          </label>
-          <label>
-            Content:
-            <input
-              type='text'
-              name='contents'
-              value={content.contents}
-              onChange={(e) => handleInputChange(index, e)}
-            />
-          </label>
-          <label>
-            Image Link:
-            <input
-              type='text'
-              name='tipImage'
-              value={content.tipImage}
-              onChange={(e) => handleInputChange(index, e)}
-            />
-          </label>
-          <button type='button' onClick={() => handleDelete(index)}>
-            Delete
+        {!isEditMode &&
+          contents.map((content, index) => (
+            <div key={index} className={style.Row}>
+              <div className={`${style.inputGroup} ${style.organizerInput}`}>
+                <label>Content Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={content.name}
+                  onChange={(e) => handleInputChange(index, e)}
+                />
+              </div>
+              <div className={`${style.inputGroup} ${style.organizerInput}`}>
+                <label>Content:</label>
+                <input
+                  type="text"
+                  name="contents"
+                  value={content.contents}
+                  onChange={(e) => handleInputChange(index, e)}
+                />
+              </div>
+              <div className={style.inputGroup}>
+                <label>Image Link:</label>
+                <input
+                  type="text"
+                  name="tipImage"
+                  value={content.tipImage}
+                  onChange={(e) => handleInputChange(index, e)}
+                />
+              </div>
+
+              <div className={`${style.inputGroup} `}>
+                <label className={style.hideLable}> a</label>
+                <button
+                  className={`${styleBtn.btn} ${style.btnTip}`}
+                  type="button"
+                  onClick={() => handleDelete(index)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
+
+        {isEditMode &&
+          contents.map((content, index) => (
+            <div key={index} className={style.Row}>
+              <div className={`${style.inputGroup} ${style.organizerInput}`}>
+                <label>Content Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={content.name}
+                  onChange={(e) => handleInputChangeUpdate(index, e)}
+                />
+              </div>
+              <div className={`${style.inputGroup} ${style.organizerInput}`}>
+                <label>Content:</label>
+                <input
+                  type="text"
+                  name="contents"
+                  value={content.contents}
+                  onChange={(e) => handleInputChangeUpdate(index, e)}
+                />
+              </div>
+              <div className={style.inputGroup}>
+                <label>Image Link:</label>
+                <input
+                  type="text"
+                  name="tipImage"
+                  value={content.tipImage}
+                  onChange={(e) => handleInputChangeUpdate(index, e)}
+                />
+              </div>
+              <div className={`${style.inputGroup} `}>
+                <label className={style.hideLable}> a</label>
+                <button
+                  className={`${styleBtn.btn} ${style.btnTip}`}
+                  type="button"
+                  onClick={() => handleDelete(index)}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
+        <div className={styleBtn.btnBottomDiv}>
+          <button
+            className={`${styleBtn.btn} ${styleBtn.btnBottom} `}
+            type="button"
+            onClick={handleAddContent}
+          >
+            Add more tip content
+          </button>
+
+          <button
+            className={`${styleBtn.btn} ${styleBtn.btnBottom}`}
+            type="submit"
+            onClick={isEditMode ? handleUpdate : hanldeSubmit}
+          >
+            {isEditMode ? "Update" : "Create"}
+          </button>
+          <button
+            className={`${styleBtn.btn} ${styleBtn.btnBottom} `}
+            type="button"
+            onClick={hanldeReturn}
+          >
+            Return
           </button>
         </div>
-      ))}
-
-      {isEditMode && contents.map((content, index) => (
-        <div key={index}>
-          <label>
-            Content Name:
-            <input
-              type='text'
-              name='name'
-              value={content.name}
-              onChange={(e) => handleInputChangeUpdate(index, e)}
-            />
-          </label>
-          <label>
-            Content:
-            <input
-              type='text'
-              name='contents'
-              value={content.contents}
-              onChange={(e) => handleInputChangeUpdate(index, e)}
-            />
-          </label>
-          <label>
-            Image Link:
-            <input
-              type='text'
-              name='tipImage'
-              value={content.tipImage}
-              onChange={(e) => handleInputChangeUpdate(index, e)}
-            />
-          </label>
-          <button type='button' onClick={() => handleDelete(index)}>
-            Delete
-          </button>
-        </div>
-      ))}
-
-      <button type='button' onClick={handleAddContent}>
-        Add more tip content
-      </button>
-
-      <button type="submit" onClick={isEditMode ? handleUpdate : hanldeSubmit}>{isEditMode ? 'Update' : 'Create'}</button>
-      <button type="button" onClick={hanldeReturn}>Return</button>
-    </form>
+      </form>
+    </div>
   );
 };
 
