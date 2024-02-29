51416d481389f33000c2d98028411964393d54bd
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-02-29 23:34:27 +0700

    hell

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
+/** @type {import('next').NextConfig} */
 const path = require('path');
 
 module.exports = {
-  images: {
-    domains: ['res.cloudinary.com'],
+//  output: 'export',
+ images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts', // Ensure this file exists and exports a loader function
  },
-  
-  webpack: (config) => {
+ webpack: (config) => {
+    // Add a rule to handle CSS files from Bootstrap
     config.module.rules.push({
       test: /bootstrap\.min\.css$/,
       use: ['style-loader', 'css-loader'],
     });
 
-    // Add an alias for '@'
+    // Add an alias for '@' to simplify imports
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
 
     return config;
-  },
-  output: 'export'
+ },
 };
