d12ae9b22930f952a5edfa3f9c7f4e0b731c5652
Nguyen Le Thu Nhan <102719300+NhanNguyen20@users.noreply.github.com>
2024-03-06 14:19:44 +0700

    update email for user

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 import styles from "@/styles/Profile.module.css";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from '@/contexts/AuthContext';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
@@ -11,28 +11,31 @@ export default function Profile() {
   // const { isLoggedIn, logout, username } = useAuth();
   const [changePw, setChangePw] = useState(false);
 
-  const {isLoggedIn}= useAuth();
+  const { isLoggedIn } = useAuth();
   const router = useRouter();
 
-  useEffect(() =>{
-    if(!isLoggedIn){
+  useEffect(() => {
+    if (!isLoggedIn) {
       router.push('/login');
     }
-  }, [isLoggedIn,router]);
-
-  
+  }, [isLoggedIn, router]);
 
   function toggleChangePw() {
     window.location.href = "/resetPassword";
+  }
+
+  function toggleChangeEmail() {
+    window.location.href = "/resetEmail";
   }
 
   return (
     <div className={styles.container}>
       <Image alt="" className={styles.img} src="/HappyGFCC.png" width={100} height={117} />
       <ClientSideUsername /> {/* Render the client-side only component */}
-      <p className={styles.p}>
-        {/* Major: <span>Business Management</span> */}
-      </p>
+
+      <button className={styles.button} onClick={toggleChangeEmail}>
+        Update your email
+      </button>
       <button className={styles.button} onClick={toggleChangePw}>
         Update your password
       </button>
