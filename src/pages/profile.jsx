ad146fafbb38eba5bd3f87f0169c51d23c339251
TaiVanNgo <vantaingo.056@gmail.com>
2024-03-31 15:50:26 +0700

    button go to admin dashboard
    in profile of admin has the button that go to admin dashboard

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,22 +1,26 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import styles from "@/styles/Profile.module.css";
 import { useState, useEffect } from "react";
-import { useAuth } from '@/contexts/AuthContext';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
+import { useAuth } from "@/contexts/AuthContext";
+import { useRouter } from "next/router";
+import Image from "next/image";
 // Dynamically import the component with ssr: false
-const ClientSideUsername = dynamic(() => import('@/components/ClientSideOnly.jsx'), { ssr: false });
+
+const ClientSideUsername = dynamic(
+  () => import("@/components/ClientSideOnly.jsx"),
+  { ssr: false }
+);
 
 export default function Profile() {
   // const { isLoggedIn, logout, username } = useAuth();
   const [changePw, setChangePw] = useState(false);
 
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, isAdmin } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
     if (!isLoggedIn) {
-      router.push('/login');
+      router.push("/login");
     }
   }, [isLoggedIn, router]);
 
@@ -28,17 +32,31 @@ export default function Profile() {
     window.location.href = "/resetEmail";
   }
 
+  function toggleAdmin() {
+    window.location.href = "/admin/dashboard/view";
+  }
+
   return (
     <div className={styles.container}>
-      <Image alt="" className={styles.img} src="/HappyGFCC.png" width={100} height={117} />
+      <Image
+        alt=""
+        className={styles.img}
+        src="/HappyGFCC.png"
+        width={100}
+        height={117}
+      />
       <ClientSideUsername /> {/* Render the client-side only component */}
-
       <button className={styles.button} onClick={toggleChangeEmail}>
         Update your email
       </button>
       <button className={styles.button} onClick={toggleChangePw}>
         Update your password
       </button>
+      {isAdmin && (
+        <button className={styles.button} onClick={toggleAdmin}>
+          Go to Admin Dashboard
+        </button>
+      )}
     </div>
   );
 }
