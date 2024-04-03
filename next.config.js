0dcf9fe6bb843d0e74e42d49b0dc6ff549331a50
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-04-03 12:16:53 +0700

    fix: fixing alias

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  images: {
+ images: {
     domains: ['res.cloudinary.com'],
-  },
+ },
 
-  webpack: (config) => {
+ webpack: (config) => {
+    // Add your custom rule for handling Bootstrap CSS
     config.module.rules.push({
       test: /bootstrap\.min\.css$/,
       use: ['style-loader', 'css-loader'],
     });
 
-    // Add an alias for '@'
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    // Merge your custom alias with the existing ones
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
 
     return config;
-  },
-  // output: 'export'     // comment this line to run localhost
+ },
+ // output: 'export'     // comment this line to run localhost
 };
