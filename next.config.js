30c421ccafa1ee8cffc6cc6bf17b6fd9b9b3bedf
phatgg221 <129394719+phatgg221@users.noreply.github.com>
2024-04-05 14:56:20 +0700

    remove export static

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@ const path = require('path');
 module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
-    unoptimized: true, // Disable Image Optimization
   },
   webpack: (config) => {
+    // Add an alias for '@'
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
     return config;
   },
-  output: 'export',
+  output: 'export', // Add this line to enable static HTML export
 };
