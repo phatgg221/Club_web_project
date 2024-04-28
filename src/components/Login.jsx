676baf8f51261d2379c19986d05799d9840b3498
TaiVanNgo <vantaingo.056@gmail.com>
2024-04-28 21:16:00 +0700

    detect the capslock when login.
    The button is shit :-1:

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,21 @@
 import styles from "../styles/Login.module.css";
-import { useState, useEffect } from 'react';
-import jwt from 'jsonwebtoken';
-import { useAuth } from '../contexts/AuthContext';
-import { useRouter } from 'next/router';
+import { useState, useEffect } from "react";
+import jwt from "jsonwebtoken";
+import { useAuth } from "../contexts/AuthContext";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
+
 export default function Login() {
   const router = useRouter();
-  const { login, isLoggedIn, isAdmin, adminLogin, highestAdminLogin } = useAuth();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const { login, isLoggedIn, isAdmin, adminLogin, highestAdminLogin } =
+    useAuth();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [formSubmit, setFormSubmit] = useState(false);
   const [isNotCorrect, setIsnotcorrect] = useState(false);
   const [admin, setAdmin] = useState([]);
+  const [capsLockOn, setCapsLockOn] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,14 +25,11 @@ export default function Login() {
 
         setAdmin(data);
       } catch (err) {
-        console.log('Error fecthing data: ', err);
+        console.log("Error fecthing data: ", err);
       }
     };
     fetchData();
   }, []);
-
-
-
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -39,12 +39,12 @@ export default function Login() {
       return;
     }
 
-    if (username === 'admin') {
+    if (username === "admin") {
       // Check if the entered password matches the admin password
       if (password === admin.data.mongoData[0].adminPassword) {
         // Perform admin login logic here, for example, redirect to admin dashboard
         highestAdminLogin();
-        router.push('/admin/dashboard/view');
+        router.push("/admin/dashboard/view");
 
         return;
       } else {
@@ -53,19 +53,18 @@ export default function Login() {
       }
     }
 
-
-    const response = await fetch('/api/login_api', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ action: 'login', username, password }),
+    const response = await fetch("/api/login_api", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ action: "login", username, password }),
     });
 
     if (response.ok) {
       try {
         const data = await response.json();
-        console.log('Data:', data.data);
+        console.log("Data:", data.data);
         const decodedToken = jwt.decode(data.data.token, { complete: true });
-        console.log('Decoded token:', decodedToken.payload.user.id);
+        console.log("Decoded token:", decodedToken.payload.user.id);
 
         const userId = decodedToken.payload.user.id;
         const isAdmin = data.data.user.isAdmin;
@@ -74,34 +73,34 @@ export default function Login() {
         // console.log(JSON.stringify(decodedToken.payload.user)+ "akjsdnajnc");
         if (userId) {
           if (isAdmin) {
-            router.push('/admin/dashboard/view');
+            router.push("/admin/dashboard/view");
             adminLogin();
             login(userId, username2);
           } else {
-            console.log('User ID:', userId);
+            console.log("User ID:", userId);
 
             login(userId, username2);
 
-            router.push('/userMain');
+            router.push("/userMain");
           }
-
-
-
-
         } else {
-          console.error('Invalid token structure');
+          console.error("Invalid token structure");
         }
       } catch (err) {
-        console.error('Token verification failed:', err);
+        console.error("Token verification failed:", err);
       }
 
-      console.log('Login successful');
+      console.log("Login successful");
     } else {
       setIsnotcorrect(true);
-      console.log('Login failed');
+      console.log("Login failed");
     }
   };
 
+  const handleCapsLock = (e) => {
+    const capsLockActivated = e.getModifierState("CapsLock");
+    setCapsLockOn(capsLockActivated);
+  };
 
   return (
     <div className={styles.mainContainer}>
@@ -129,23 +128,45 @@ export default function Login() {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-          {formSubmit && !username && <p style={{ color: 'red' }}>Username is required</p>}
-          <input
-            type="password"
-            id={styles.password}
-            require='true'
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          {formSubmit && !username && (
+            <p style={{ color: "red" }}>Username is required</p>
+          )}
+          <div className={styles.passwordContainer}>
+            <input
+              type="password"
+              id={styles.password}
+              required={true} // It should be 'required' not 'require'
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onKeyUp={handleCapsLock}
+              onKeyDown={handleCapsLock}
+            />
+            {capsLockOn && (
+              <Image
+                src="/capslockIcon3.png"
+                width={30}
+                height={50}
+                alt="Caps lock is on"
+              />
+            )}
+          </div>
+
           <Link href="/forgotPassword">Forgot Password?</Link>
-          {formSubmit && !username && <p style={{ color: 'red' }}>Password is required</p>}
-          {isNotCorrect && <p style={{ color: 'red' }}> Wrong username or password</p>}
-          <button type="submit" className={styles.loginButton} onClick={handleSubmit}>
+          {formSubmit && !username && (
+            <p style={{ color: "red" }}>Password is required</p>
+          )}
+          {isNotCorrect && (
+            <p style={{ color: "red" }}> Wrong username or password</p>
+          )}
+          <button
+            type="submit"
+            className={styles.loginButton}
+            onClick={handleSubmit}
+          >
             Login
           </button>
-          <p className={styles.forgotPasswordLink}>
-          </p>
+          <p className={styles.forgotPasswordLink}></p>
         </form>
       </div>
     </div>
